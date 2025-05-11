bb47f18621e384830f5c617c757d254cd3e6e277
anggagumilang212 <anggagumilang212@gmail.com>
2025-05-11 22:44:23 +0700

    nav file manager

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -86,6 +86,12 @@
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
             </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('file-manager')" :active="request()->routeIs('file-manager')">
+                {{ __('File manager') }}
+            </x-responsive-nav-link>
+            {{-- <x-responsive-nav-link :href="route('profile.edit')" :active="request()->routeIs('profile.edit')">
+                {{ __('Profile') }}
+            </x-responsive-nav-link> --}}
         </div>
 
         <!-- Responsive Settings Options -->
